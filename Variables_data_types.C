2a97973809df94cce6d3aec8820d89f15263faaf
Jai Krishna <49020643+Jkthehero99@users.noreply.github.com>
2019-12-06 05:49:02 +0530

    format specifiers

--- Variables_data_types.C
+++ Variables_data_types.C
@@ -5,14 +5,15 @@ Discription:A program to show the use of enums, format specifiers and
 */
 
 #include<stdio.h>
-extern int var;
 int main()
 {
+    int g=23456;
    static int t7=5;
     __int64 t;  
     __int32 t2;
     register __int64 t5;
-    printf("Enter a value %",var);
+    printf("g is equal to %+7.3lx in hexadecimal and is left indexed \n",g);
+    printf("Enter a value ");
     scanf("%lld",&t);
     printf("you entered %lld and %d %d %lld %d",t,sizeof(t),sizeof(t2),__INT64_MAX__,sizeof(t5));
     printf("\n static int value is %d and new value is %d",t7,t7+5);
