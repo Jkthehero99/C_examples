723f13a365ea6a7844abb76233552569a461135d
Jai Krishna <49020643+Jkthehero99@users.noreply.github.com>
2019-12-06 05:42:37 +0530

    A Program on Command Line arguments

---
+++ Command_Line_Arg.C
@@ -0,0 +1,15 @@
+/*
+Author:B.Jai Krishna
+Init Date:5-12-19
+Description: A simple program to demonstrate CLA. 
+*/
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+printf("Number of arguments passed is %d \n",argc);
+
+printf("The name of the program is %s \n",argv[0]);
+printf("The argument passed is %s",argv[1]);
+
+}
